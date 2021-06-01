9cba5fe4613ab51499ff85a0643fa99c226f973e
mariahynes <maria.m.hynes@gmail.com>
2021-06-01 10:37:29 +0000

    Update formatting. Add js file

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+class Numbers {
+    
+    num_array=[1,2,3,4,5,6,7,8,9,10,11,12];
+
+    print_numbers() {
+         console.log(this.num_array);
+    }
+
+    addition_array() {
+
+    }
+
+}
+
+let the_operands = new Numbers();
+the_operands.print_numbers();
